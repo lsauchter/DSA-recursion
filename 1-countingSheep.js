ffd55983104ab34a34df918ef2e4869b45588ddc
Lauren Auchter <lsauchter@gmail.com>
2019-09-26 17:11:12 -0400

    Counting sheep

---
+++ 1-countingSheep.js
@@ -0,0 +1,9 @@
+const countSheep = function(num) {
+    if (num === 0) {
+      return console.log('All sheep jumped over the fence')
+    }
+    console.log(`${num}: Another sheep jumps over the fence`)
+    countSheep((num - 1))
+}
+  
+countSheep(4)
